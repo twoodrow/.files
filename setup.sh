b19aeb1e2fad06b319557cdd427a1db602b2f9f0
Travis Woodrow <traviswoodrow@email.arizona.edu>
2016-02-01 11:07:24 -0700

    Fixes symlinking and adds install of pip's neovim package for YouCompleteMe support

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-ln -s ./nvim ~/.config/nvim
-ln -s /usr/bin/nvim /usr/local/bin/nv
 
 # installing neovim
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y neovim
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip cmake build-essential
+sudo pip install neovim
 
 # use neovim instead of other editor things
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
@@ -16,3 +15,16 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
+
+# setup symbolic linking
+## make directories
+USER_DIR=/home/$USER
+NV_DIR=$USER_DIR/.config/nvim
+echo user directory: $USER_DIR
+echo neovim directory: $NV_DIR
+sudo mkdir -p $NV_DIR/
+sudo mkdir -p $NV_DIR/autoload
+# make links
+sudo ln -s $USER_DIR/dotfiles/nvim/init.vim $NV_DIR/init.vim
+sudo ln -s $USER_DIR/dotfiles/nvim/autoload/plug.vim $NV_DIR/autoload/plug.vim
+sudo ln -s /usr/bin/nvim /usr/local/bin/nv
