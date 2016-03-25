e5eb4a25f1ec29bc9f0af1abd1a666a9907b057b
Travis <traviswoodrow@email.arizona.edu>
2016-03-25 15:38:17 -0700

    fixes permissions of links

--- setup.sh
+++ setup.sh
@@ -12,6 +12,20 @@ sudo apt-get install -y neovim
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip cmake build-essential
 sudo pip install neovim
 
+# setup symbolic linking
+## make directories
+USER_DIR=/home/$USER
+NV_DIR=$USER_DIR/.config/nvim
+echo user directory: $USER_DIR
+echo neovim directory: $NV_DIR
+mkdir -p $NV_DIR/
+mkdir -p $NV_DIR/autoload
+
+# make links
+ln -s $USER_DIR/.files/nvim/init.vim $NV_DIR/init.vim
+ln -s $USER_DIR/.files/nvim/autoload/plug.vim $NV_DIR/autoload/plug.vim
+ln -s /usr/bin/nvim /usr/local/bin/nv
+
 # use neovim instead of other editor things
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
@@ -19,16 +33,3 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
-
-# setup symbolic linking
-## make directories
-USER_DIR=/home/$USER
-NV_DIR=$USER_DIR/.config/nvim
-echo user directory: $USER_DIR
-echo neovim directory: $NV_DIR
-sudo mkdir -p $NV_DIR/
-sudo mkdir -p $NV_DIR/autoload
-# make links
-sudo ln -s $USER_DIR/dotfiles/nvim/init.vim $NV_DIR/init.vim
-sudo ln -s $USER_DIR/dotfiles/nvim/autoload/plug.vim $NV_DIR/autoload/plug.vim
-sudo ln -s /usr/bin/nvim /usr/local/bin/nv
